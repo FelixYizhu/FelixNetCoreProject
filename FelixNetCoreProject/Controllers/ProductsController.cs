8db49d5f9fd25f999e36553aa439f6fbcca0d420
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-09 01:08:44 +1000

    Add files via upload

--- FelixNetCoreProject/Controllers/ProductsController.cs
+++ FelixNetCoreProject/Controllers/ProductsController.cs
@@ -23,5 +23,15 @@ namespace FelixNetCoreProject.Controllers
         {
             return ProductService.GetProducts();
         }
+        // [HttpPatch] "FromBody"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
     }
 }
