d455ced81aaf81cd793aebe2f2a60c9d08dde38d
Felix <49159678+FelixYizhu@users.noreply.github.com>
2020-08-09 00:38:09 +1000

    modify ProductsController.cs file
    modify ProductsController.cs file

--- FelixNetCoreProject/Controllers/ProductsController.cs
+++ FelixNetCoreProject/Controllers/ProductsController.cs
@@ -1,1 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FelixNetCoreProject.Models;
+using FelixNetCoreProject.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
+namespace FelixNetCoreProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
